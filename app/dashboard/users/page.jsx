957d9fa21c9ae6450537bdf3fed2b8769a4284da
Umut Bektaş <umutbektass.78@hotmail.com>
2024-06-28 17:47:19 +0300

    first commit

---
+++ app/dashboard/users/page.jsx
@@ -0,0 +1,8 @@
+const UsersPage = ()=>{
+
+    return (
+        <div>Users Page</div>
+    )
+}
+
+export default UsersPage
---
+++ app/ui/dashboard/sidebar/sidebar.jsx
@@ -0,0 +1,11 @@
+import styles from '@/app/ui/dashboard/sidebar/sidebar.module.css'
+const Sidebar = () => {
+
+    return (
+        <div className={styles.container}>
+            Sidebar
+        </div>
+    )
+}
+
+export default Sidebar
